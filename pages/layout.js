972fb0e8fb0e3a3d0f68abbb8d3450459e6eec52
LakshmiPrasad Bathina <blakshmi.prasad@cbre.com>
2023-11-17 09:43:05 +0530

    added publishable key for ClerkProvider

--- pages/layout.js
+++ pages/layout.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <html lang='en'>
         <body className={inter.className}>{children}</body>
       </html>
